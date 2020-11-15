8f224a2326d46f2d60d9f649a6edf5868b670bc2
mie378 <73713814+mie378@users.noreply.github.com>
2020-11-15 01:45:09 +0100

    Add files via upload

--- src/CatTable.vue
+++ src/CatTable.vue
@@ -1,17 +1,17 @@
 <template>
 	<div v-if="cats.length > 0">
 			<div class="row">
-				<div class="col-sm-6 col-md-3"><h3>kot</h3></div>
-				<div class="col-sm-6 col-md-3"><h3>karma</h3></div>
+				<div class="col-6 col-md-3"><h3>kot</h3></div>
+				<div class="col-6 col-md-3"><h3>karma</h3></div>
 			</div>
 			<div v-for="cat2 in cats" v-bind:key="cat2.id">
 				<div class="row">
-					<div class="col-sm-6 col-md-3">{{cat2.cat}}</div> 
-					<div class="col-sm-6 col-md-3">{{cat2.food}}</div>
-					<div class="col-sm-6 col-md-3">
+					<div class="col-6 col-md-3">{{cat2.cat}}</div> 
+					<div class="col-6 col-md-3">{{cat2.food}}</div>
+					<div class="col-6 col-md-3">
 						<button v-on:click="deletec(cat2.id)" type="button" class="btn btn-danger">Usuń</button>
 					</div>
-					<div class="col-sm-6 col-md-3">
+					<div class="col-6 col-md-3">
 						<button v-on:click="edit(cat2.id)" type="button" class="btn btn-success">Edytuj</button>
 					</div>
 					
